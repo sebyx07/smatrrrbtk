50ba889639764609188fcd636b79d874f3cdf81b
sebi <gore.sebyx@yahoo.com>
2017-06-07 15:15:09 +0300

    add the currencyservice

--- app/app/services/currency_service.rb
+++ app/app/services/currency_service.rb
@@ -1,10 +1,16 @@
 module CurrencyService
   RESULT = Struct.new(:date, :value)
-  class HttpErr < Exception; end
+  class CurrencyErr < Exception; end
+  CURRENCIES_CLASSES = {btc: CurrencyService::Btc}
 
   class << self
     def get_currency(currency, time_span)
+      klass = CURRENCIES_CLASSES[currency]
+      if klass.nil?
+        raise CurrencyErr.new('currency not available')
+      end
 
+      klass.get(time_span)
     end
   end
 end
---
+++ app/spec/services/currency_service_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe CurrencyService do
+  describe 'get_currency' do
+    context 'unknown currency' do
+      it 'throws' do
+        expect do
+          CurrencyService.get_currency(:ron, '30')
+        end.to raise_error CurrencyService::CurrencyErr
+      end
+    end
+
+    context 'valid currencies' do
+      [:btc].each do |currency|
+        it 'ok' do
+          values = CurrencyService.get_currency(currency, '30')
+          expect(values).not_to be_nil
+        end
+      end
+    end
+  end
+end
